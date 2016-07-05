a7adf31009815010bcdf3214b7a257e4a4fee725
Florian Diegruber <florian@diegruber.net>
2016-07-05 12:24:22 +0200

    add seeds to my app

--- Week4/timetracking/db/seeds.rb
+++ Week4/timetracking/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Project.create(name:"Crazy project", description: "you wont believe it")
+Project.create(name:"Time tracking app")
+Project.create(name:"Recipees", description: "for all the foodies")
+Project.create(name:"Crazy project1", description: "you wont believe it1")
+Project.create(name:"Time tracking app1")
+Project.create(name:"Recipees1", description: "for all the foodies1")
+Project.create(name:"Crazy project2", description: "you wont believe it2")
+Project.create(name:"Time tracking app2")
+Project.create(name:"Recipees2", description: "for all the foodies2")
+Project.create(name:"Crazy project3", description: "you wont believe it3")
